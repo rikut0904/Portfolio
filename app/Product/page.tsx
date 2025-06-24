64494c54d6b1fbf288bddd10dd14793e96e7d7de
rikut0904 <rkt9tuba4robot@gmail.com>
2025-06-24 10:37:32 +0900

    各ページテンプレート作成

---
+++ app/Product/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from "react";
+import FadeInSection from "../components/FadeInSection";
+
+export default function ActivityPage() {
+    return (
+        <main className="max-w-5xl mx-auto p-6">
+            <FadeInSection>
+                <section id="activity" className="py-8">
+                    <h1>制作物</h1>
+                </section>
+            </FadeInSection>
+        </main>
+    );
+}
