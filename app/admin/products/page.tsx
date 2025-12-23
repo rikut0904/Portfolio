2363a57894ab90f2419c842fc77844cdaee4d8a1
rikut0904 <rkt9tuba4robot@gmail.com>
2025-12-23 16:32:02 +0900

    fix/admin_page_darkMode

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -104,7 +104,7 @@ function ProductsContent() {
 
   if (loading) {
     return (
-      <div className="min-h-screen flex items-center justify-center">
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
         <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600"></div>
       </div>
     );
