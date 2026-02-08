2d7eb1b9c0ae4497400bfa57e0274343467716f8
rikut0904 <rkt9tuba4robot@gmail.com>
2026-02-08 15:46:29 +0900

    add/アナリティクス

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,19 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
+      <head>
+        <script defer src="https://cloud.umami.is/script.js" data-website-id="21b7a828-5f59-42e8-94b6-8c62fd190fd5"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-JTH2XG05YV"></script>
+        <script>
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-JTH2XG05YV');
+          `}
+        </script>
+      </head>
       <body>
         <AuthProvider>{children}</AuthProvider>
       </body>
